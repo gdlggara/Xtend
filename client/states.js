4bbf9696bf82b4484581ec0e28833e791bceee93
Guillermo Garate Amaral <guillermo.garate@flex.com>
2017-09-07 09:45:16 -0700

    [Studio] Add screen "bossroutingviewer"

--- client/states.js
+++ client/states.js
@@ -42,5 +42,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.bossroutingviewer', {
+        views: {
+            app: {
+                controller: 'app_bossroutingviewer',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.bossroutingviewer');
+                }
+            }
+        }
     });
 });
